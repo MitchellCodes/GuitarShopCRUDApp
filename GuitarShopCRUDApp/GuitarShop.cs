6a8b73f3ed814f551ee01446bb769db8a47d6817
Mary Saray <msaray.ms@outlook.com>
2021-08-10 15:18:49 -0700

    Query syntax to get a list of all products

--- GuitarShopCRUDApp/GuitarShop.cs
+++ GuitarShopCRUDApp/GuitarShop.cs
@@ -16,5 +16,17 @@ namespace GuitarShopCRUDApp
         {
             InitializeComponent();
         }
+
+        private void GuitarShop_Load(object sender, EventArgs e)
+        {
+            // instance of database context class to talk to database
+            GuitarShopContext context = new GuitarShopContext();
+
+            // pull all products out of the database: Query syntax
+            List<Product> allProducts =
+                (from product in context.Products // wrap with () to get list of products
+                 orderby product.ProductName
+                 select product).ToList();
+        }
     }
 }
