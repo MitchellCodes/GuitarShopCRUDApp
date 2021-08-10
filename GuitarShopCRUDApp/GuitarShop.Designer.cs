5d294c559962c8aca2578199bb2212eb75a87af5
Mary Saray <msaray.ms@outlook.com>
2021-08-09 20:56:19 -0700

    Added combo box and renamed form

---
+++ GuitarShopCRUDApp/GuitarShop.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace GuitarShopCRUDApp
+{
+    partial class GuitarShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboCustomer = new System.Windows.Forms.ComboBox();
+            this.cboProduct = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name:";
+            // 
+            // cboCustomer
+            // 
+            this.cboCustomer.FormattingEnabled = true;
+            this.cboCustomer.Location = new System.Drawing.Point(199, 56);
+            this.cboCustomer.Name = "cboCustomer";
+            this.cboCustomer.Size = new System.Drawing.Size(376, 33);
+            this.cboCustomer.TabIndex = 2;
+            // 
+            // cboProduct
+            // 
+            this.cboProduct.FormattingEnabled = true;
+            this.cboProduct.Location = new System.Drawing.Point(199, 223);
+            this.cboProduct.Name = "cboProduct";
+            this.cboProduct.Size = new System.Drawing.Size(376, 33);
+            this.cboProduct.TabIndex = 3;
+            // 
+            // GuitarShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboProduct);
+            this.Controls.Add(this.cboCustomer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GuitarShop";
+            this.Text = "Guitar Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboCustomer;
+        private System.Windows.Forms.ComboBox cboProduct;
+    }
+}
+
--- GuitarShopCRUDApp/GuitarShop.cs
+++ GuitarShopCRUDApp/GuitarShop.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GuitarShopCRUDApp
 {
-    public partial class Form1 : Form
+    public partial class GuitarShop : Form
     {
-        public Form1()
+        public GuitarShop()
         {
             InitializeComponent();
         }
