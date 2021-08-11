1258d2150560145728a0a62c05f4e199db367486
Mary Saray <82470838+marysaray@users.noreply.github.com>
2021-08-10 19:10:29 -0700

    Issue#14 create add product form (#18)
    * Initiated add product form
    
    * Initiated add product form
    
    * Added labels and textboxes
    
    * Renamed labels
    
    * named each textboxes
    
    * Added and named button
    
    * renamed to frmGuitarShop and linked add product button to frmAddProduct
    
    * Added cancel button to frmAddProduct
    
    * Added dialog result to cancel button.

---
+++ GuitarShopCRUDApp/frmAddProduct.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace GuitarShopCRUDApp
+{
+    partial class frmAddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProductCode = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtListPrice = new System.Windows.Forms.TextBox();
+            this.txtDiscPercent = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Code:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(308, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Description:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "List Price:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 349);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(364, 50);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Discount Percent:";
+            // 
+            // txtProductCode
+            // 
+            this.txtProductCode.Location = new System.Drawing.Point(240, 19);
+            this.txtProductCode.Name = "txtProductCode";
+            this.txtProductCode.Size = new System.Drawing.Size(276, 31);
+            this.txtProductCode.TabIndex = 5;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(240, 88);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(276, 31);
+            this.txtProductName.TabIndex = 6;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(240, 170);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(276, 31);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // txtListPrice
+            // 
+            this.txtListPrice.Location = new System.Drawing.Point(240, 260);
+            this.txtListPrice.Name = "txtListPrice";
+            this.txtListPrice.Size = new System.Drawing.Size(276, 31);
+            this.txtListPrice.TabIndex = 8;
+            // 
+            // txtDiscPercent
+            // 
+            this.txtDiscPercent.Location = new System.Drawing.Point(240, 346);
+            this.txtDiscPercent.Name = "txtDiscPercent";
+            this.txtDiscPercent.Size = new System.Drawing.Size(276, 31);
+            this.txtDiscPercent.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(268, 425);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(248, 45);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(29, 425);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(162, 45);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // frmAddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 482);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDiscPercent);
+            this.Controls.Add(this.txtListPrice);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductCode);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAddProduct";
+            this.Text = "frmAddProduct";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProductCode;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtListPrice;
+        private System.Windows.Forms.TextBox txtDiscPercent;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ GuitarShopCRUDApp/frmAddProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuitarShopCRUDApp
+{
+    public partial class frmAddProduct : Form
+    {
+        public frmAddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            // show message box to confirm cancel
+            DialogResult result =
+                MessageBox.Show(
+                    "Are you sure you want to cancel?",
+                    "Cancel Product",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+            else 
+            {
+                Close();
+            }
+        }
+    }
+}
