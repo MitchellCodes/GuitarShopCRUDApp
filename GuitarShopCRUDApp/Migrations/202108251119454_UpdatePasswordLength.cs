17dc0b85d1e9daa130aea051eb987a77a4ac7d83
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-25 04:24:21 -0700

    Added migration to update database to store password as varchar(64)

---
+++ GuitarShopCRUDApp/Migrations/202108251119454_UpdatePasswordLength.cs
@@ -0,0 +1,18 @@
+﻿namespace GuitarShopCRUDApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatePasswordLength : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Password", c => c.String(nullable: false, maxLength: 64, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Password", c => c.String(nullable: false, maxLength: 60, unicode: false));
+        }
+    }
+}
