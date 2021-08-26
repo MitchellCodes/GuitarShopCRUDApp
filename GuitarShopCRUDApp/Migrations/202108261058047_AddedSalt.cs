2098813e0c1c45bfa43fcff81abde750663372f6
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-26 04:00:25 -0700

    Added Salt column to database in Customers table

---
+++ GuitarShopCRUDApp/Migrations/202108261058047_AddedSalt.cs
@@ -0,0 +1,18 @@
+﻿namespace GuitarShopCRUDApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedSalt : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Salt", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Salt");
+        }
+    }
+}
